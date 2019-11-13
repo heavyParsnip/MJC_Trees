11e50883963bec2008bc857fdcd597438ecc44b3
Matthew Camera <mjc9536@rit.edu>
2019-11-13 12:23:31 -0500

    PE completed

---
+++ MJC_Trees/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MJC_Trees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate nodes
+            TalentTreeNode magic = new TalentTreeNode("Magic", true);
+            TalentTreeNode fireball = new TalentTreeNode("Fireball", true);
+            TalentTreeNode crazyBigFireball = new TalentTreeNode("Crazy Big Fireball", false);
+            TalentTreeNode oneThousandTinyFireballs = new TalentTreeNode("1000 Tiny Fireballs", true);
+            TalentTreeNode magicArrow = new TalentTreeNode("Magic Arrow", false);
+            TalentTreeNode iceArrow = new TalentTreeNode("Ice Arrow", false);
+            TalentTreeNode explodingArrow = new TalentTreeNode("Exploding Arrow", false);
+
+            //Set relationships
+            magic.LeftChild = fireball;
+            magic.RightChild = magicArrow;
+            fireball.LeftChild = crazyBigFireball;
+            fireball.RightChild = oneThousandTinyFireballs;
+            magicArrow.LeftChild = iceArrow;
+            magicArrow.RightChild = explodingArrow;
+
+            //Call methods on root node
+            Console.WriteLine("--Listing all ablities in the game--");
+            magic.ListAllAbilities();
+            Console.WriteLine();
+
+            Console.WriteLine("--Listing all learned ablities--");
+            magic.ListKnownAbilities();
+            Console.WriteLine();
+
+            Console.WriteLine("--Listing all ablities that can currently be learned--");
+            magic.ListPossibleAbilities();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MJC_Trees/TalentTreeNode.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MJC_Trees
+{
+    class TalentTreeNode
+    {
+        //FIELDS
+        private string abilName;
+        private bool learned;
+        private TalentTreeNode leftChild;
+        private TalentTreeNode rightChild;
+
+        //PROPERTIES
+        public TalentTreeNode LeftChild
+        {
+            get { return leftChild; }
+            set { leftChild = value; }
+        }
+        public TalentTreeNode RightChild
+        {
+            get { return rightChild; }
+            set { rightChild = value; }
+        }
+
+        //CONSTRUCTORS
+        public TalentTreeNode(string abilName, bool learned)
+        {
+            this.abilName = abilName;
+            this.learned = learned;
+        }
+
+        //METHODS
+        public void ListAllAbilities()
+        {
+            //Check for a child on the left branch
+            if (leftChild != null)
+            {
+                leftChild.ListAllAbilities();
+            }
+
+            //Print this ability
+            Console.WriteLine(abilName);
+
+            //Check for a child on the right branch
+            if (rightChild != null)
+            {
+                rightChild.ListAllAbilities();
+            }
+        }
+
+        public void ListKnownAbilities()
+        {
+            if (learned == true)
+            {
+                //Print this ability
+                Console.WriteLine($"Known ability: {abilName}");
+            }
+
+            //Check for a child on the left branch
+            if (leftChild != null && learned == true)
+            {
+                leftChild.ListKnownAbilities();
+            }
+
+            //Check for a child on the right branch
+            if (rightChild != null && learned == true)
+            {
+                rightChild.ListKnownAbilities();
+            }
+        }
+
+        public void ListPossibleAbilities()
+        {
+
+            if (learned == true)
+            {
+                //Check for a child on the left branch
+                if (leftChild != null)
+                {
+                    leftChild.ListPossibleAbilities();
+                }
+            }
+            
+            if (learned == false)
+            {
+                //Print this ability
+                Console.WriteLine($"Possible ability: {abilName}");
+            }
+
+            if (learned == true)
+            {
+                //Check for a child on the right branch
+                if (rightChild != null)
+                {
+                    rightChild.ListPossibleAbilities();
+                }
+            }
+        }
+    }
+}
